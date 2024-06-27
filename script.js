3b74a8530fb8ed4e02230c854065ab4494de2bc1
Samuel Johnny <johnnysamuel486@gmail.com>
2024-06-27 09:48:35 -0300

    Atualizada as mensagens do sistema

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 import { turboLogger } from "./turboLogger.js";
 console.log("Estou usando o gitHub");
 turboLogger("Estou aprendendo o fluxo do git", "red");
+turboLogger(
+  "Eu consegui alterar o meu codigo sem afetar a versão principal do sistema",
+  "darkgreen"
+);
